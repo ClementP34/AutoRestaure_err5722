88de1d7c8b7511e23e6b3e6440070ffe48a549f1
Clement P <galiper34@gmail.com>
2017-03-06 14:45:00 +0100

    Update AutoRestaure_err5722.ps1

--- AutoRestaure_err5722.ps1
+++ AutoRestaure_err5722.ps1
@@ -1,9 +1,11 @@
 #Trouve la derniere entrée pour l'erreur Netlogon 5722
 $event = Get-EventLog -LogName "System" -source "NETLOGON" -InstanceId 5722 -EntryType Error -Newest 1
 #Recupération de l'ordinateur impacté
-$PcProbleme = $event.ReplacementStrings.Get(0)
+$NamePcProbleme = $event.ReplacementStrings.Get(0)
 #Recuperation du serveur AD
-$ServerDC = $event.MachineName
+$NameDC = $event.MachineName.Substring(0,$event.MachineName.IndexOf("."))
+#Recuperation Domaine
+$NameDomaine = $event.MachineName.Substring($event.MachineName.IndexOf(".")+1,$event.MachineName.Length-$event.MachineName.IndexOf(".")-1)
 
 #Lance la commande de reinitialisation du mot de passe machine a distance sur la machine impacté.
-Invoke-Command -ComputerName $PcProbleme -ScriptBlock {Reset-ComputerMachinePassword -Server $ServerDC}
+Invoke-Command -ComputerName $NamePcProbleme -ScriptBlock {Reset-ComputerMachinePassword -Server $NameDC}
